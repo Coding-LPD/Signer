ade1f9d922d0b241a5d782a0bb8f4745b174de06
Coding-LPD <gd4455012@qq.com>
2016-09-21 16:45:43 +0800

    add sign-record.model

--- src/app/shared/models/index.ts
+++ src/app/shared/models/index.ts
@@ -3,3 +3,4 @@ export * from './sign.model';
 export * from './sign-student.model';
 export * from './teacher.model';
 export * from './user.model';
+export * from './sign-record.model';
---
+++ src/app/shared/models/sign-record.model.ts
@@ -0,0 +1,12 @@
+export class SignRecord {
+
+  constructor(
+    public _id?: string,
+    public signId?: string,
+    public studentId?: string,
+    public studentName?: string,
+    public distance?: number,
+    public state?: number
+  ) {}
+
+}
