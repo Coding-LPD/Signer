d6f0df8cc5a8b5a23ae0c4c57f011d1ab0c4d936
Coding-LPD <gd4455012@qq.com>
2016-09-21 13:15:44 +0800

    add student.model

---
+++ src/app/shared/models/student.model.ts
@@ -0,0 +1,18 @@
+export class Student {
+
+  constructor(
+    public _id?: string,
+    public phone?: string,
+    public number?: string,
+    public name?: string,
+    public gender?: string,
+    public school?: string,
+    public academy?: string,
+    public major?: string,
+    public grade?: string,
+    public _class?: string,
+    public mail?: string,
+    public avatar?: string  
+  ) {}
+
+}
