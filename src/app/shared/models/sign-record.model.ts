74fd888b43754e018fa3ef79d4270392ba4c58bc
Coding-LPD <gd4455012@qq.com>
2016-09-22 17:13:03 +0800

    update sign-record.model

--- src/app/shared/models/sign-record.model.ts
+++ src/app/shared/models/sign-record.model.ts
@@ -3,10 +3,15 @@ export class SignRecord {
   constructor(
     public _id?: string,
     public signId?: string,
+    public phoneId?: string,
     public studentId?: string,
     public studentName?: string,
+    public studentAvatar?: string,    
     public distance?: number,
-    public state?: number
+    public state?: number,
+    public type?: number,
+    public createdAt?: string,
+    public battery?: number      
   ) {}
 
 }
