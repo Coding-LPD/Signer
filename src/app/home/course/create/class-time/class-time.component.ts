ebc768fe0646b813035cfed96bff4cfda8a2b938
Coding-LPD <gd4455012@qq.com>
2016-10-21 15:16:36 +0800

    modify class-time component

--- src/app/home/course/create/class-time/class-time.component.ts
+++ src/app/home/course/create/class-time/class-time.component.ts
@@ -10,14 +10,28 @@ export class ClassTimeComponent {
   day = '星期一';
   startIndex = 1;
   endIndex = 1;
+  minIndex = 1;
+  maxIndex = 11;
+  // 用于生成option选项
+  options = new Array(this.maxIndex);
   
+  // 时间串格式为: "星期一 4节-8节"
   @Input() set time(t: string) {
+    // 时间串为空
     if (!t.trim()) return;
-    var times = t.split(' ');
-    var indexs = times[1].split('-');   
-    this.day = times[0];    
-    this.startIndex = Number(indexs[0].slice(0, 1));
-    this.endIndex = Number(indexs[1].slice(0, 1));
+    
+    // 时间串格式不正确
+    var times = t.split(' ');    
+    if (times.length != 2) return;
+
+    // 节数如果有负数，则split之后元素数会大于2
+    var indexs = times[1].split('-');    
+    if (indexs.length != 2) return;
+
+    this.day = times[0];
+    this.startIndex = Number(indexs[0].slice(0, -1));
+    this.endIndex = Number(indexs[1].slice(0, -1));        
+    this.checkIndex();
   }
 
   @Output() timeChange = new EventEmitter<string>();
@@ -28,28 +42,33 @@ export class ClassTimeComponent {
   }
 
   selectStartIndex(startIndex: number) {
-    // 保证开始节数小于结束节数
-    if (startIndex <= this.endIndex) {
-      this.startIndex = startIndex;
-    } else {
-      this.startIndex = this.endIndex;
-      this.endIndex = startIndex;
-    }
+    this.startIndex = startIndex;
+    this.checkIndex();
     this.timeChange.emit(this.getTimeString());
   }
 
   selectEndIndex(endIndex: number) {
-    // 保证开始节数小于结束节数
-    if (this.startIndex <= endIndex) {
-      this.endIndex = endIndex;
-    } else {
-      this.endIndex = this.startIndex;
-      this.startIndex = endIndex;
-    }
+    this.endIndex = endIndex;
+    this.checkIndex();
     this.timeChange.emit(this.getTimeString());
   }
 
-  getTimeString(): string {
+  private getTimeString(): string {
     return `${this.day} ${this.startIndex}节-${this.endIndex}节`;
+  }
+
+  // 保证节数不会超过限定范围，并且起始节数不大于结束节数
+  private checkIndex() {
+    if (this.startIndex < this.minIndex || this.startIndex > this.maxIndex) {
+      this.startIndex = this.minIndex;
+    }
+    if (this.endIndex < this.minIndex || this.endIndex > this.maxIndex) {
+      this.endIndex = this.minIndex;
+    }
+    if (this.endIndex < this.startIndex) {
+      var t = this.startIndex;
+      this.startIndex = this.endIndex;
+      this.endIndex = t;
+    }
   }
 }
