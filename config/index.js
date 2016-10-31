87724ea1c32a50657ddba6344f27e27a829b7cdd
Coding-LPD <gd4455012@qq.com>
2016-10-31 10:50:15 +0800

    change log info to english

--- config/index.js
+++ config/index.js
@@ -6,8 +6,8 @@ env = env.toLowerCase();
 var file = path.resolve(__dirname, env);
 try {
   var config = module.exports = require(file);
-  console.log('加载配置文件：%s.js', env);
+  console.log('load: %s.js', env);
 } catch (err) {
-  console.error('加载配置文件 %s.js 失败', env);
+  console.error('load fail: %s.js', env);
   throw err;
 }
