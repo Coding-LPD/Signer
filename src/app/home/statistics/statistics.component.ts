fddec64773e486122d533d4cfb6eef6fce11b4d8
Coding-LPD <gd4455012@qq.com>
2016-11-12 12:56:05 +0800

    add custom course select

--- src/app/home/statistics/statistics.component.ts
+++ src/app/home/statistics/statistics.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class StatisticsComponent {
 
+  isOpenSelect = false;
+  selectedCourse = '';  
+  courses: string[] = ['软件工程软件工程软件工程软件工程', '安卓程序设计', 'iOS程序设计'];
+
+  toggleSelect() {
+    this.isOpenSelect = !this.isOpenSelect;
+  }
+
+  selectCourse(course: string) {
+    this.isOpenSelect = false;
+    this.selectedCourse = course;
+  }
+
 }
