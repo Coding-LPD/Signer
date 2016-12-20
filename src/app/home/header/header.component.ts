63b428fcb0e326e636ac1335088868698d97c930
Coding-LPD <gd4455012@qq.com>
2016-12-20 11:35:35 +0800

    display true personal info in header

--- src/app/home/header/header.component.ts
+++ src/app/home/header/header.component.ts
@@ -1,15 +1,34 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { LoginService } from '../../login';
+import { Teacher } from '../../shared';
 
 @Component({
   selector: 'header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   
+  teacher: Teacher;
   hideMenu = true;
   showSidebar = true;
   @Output() onToggleSidebar = new EventEmitter<boolean>();
+
+  constructor(
+    private _router: Router,
+    private _loginService: LoginService) {}
+
+
+  ngOnInit() {
+    this._loginService.getTeacherInfo()
+      .subscribe(body => {
+        if (+body.code == 200) {
+          this.teacher = body.data[0];
+        }
+      });
+  }
 
   toggleMenu() {
     this.hideMenu = !this.hideMenu;    
@@ -18,6 +37,10 @@ export class HeaderComponent {
   toggleSidebar() {
     this.showSidebar = !this.showSidebar;
     this.onToggleSidebar.emit(this.showSidebar);
+  }
+
+  logout() {
+    this._router.navigate(['/home/login']);
   }
 
 }
