b1b62e7cf86edcc563db19ebe3b1cfc80fe9ed0d
Coding-LPD <gd4455012@qq.com>
2017-01-08 09:48:22 +0800

    add teacher sign in period api

--- routes/teacher.js
+++ routes/teacher.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var moment = require('moment');
 var router = express.Router();
 
 var handleErrors = require('../services/error-handler').handleErrors;
@@ -6,6 +7,7 @@ var sendInfo = require('../services/error-handler').sendInfo;
 var errorCodes = require('../services/error-codes').errorCodes;
 var common = require('../services/common');
 var Teacher = require('../services/mongo').Teacher;
+var Sign = require('../services/mongo').Sign;
 var ChatRoom = require('../services/mongo').ChatRoom;
 var ChatMsg = require('../services/mongo').ChatMsg;
 
@@ -66,6 +68,47 @@ router.post('/search', function (req, res) {
       handleErrors(err, res, []);
     }
   });
+});
+
+router.post('/:id/signsInPeriod', function (req, res) {
+  var teacherId = req.params['id'];
+  var format = 'YYYY-MM-DD HH:mm:ss';
+  var startTime, endTime;
+
+  Promise.resolve()
+    .then(function () {      
+      startTime = moment(req.body.startTime).format(format);
+      endTime = moment(req.body.endTime).add(1, 'day').format(format);
+
+      // 查询在这段时间内的签到数据，包含开始时间和结束时间
+      return Sign.find({ teacherId: teacherId, startTime: { $gt: startTime, $lt: endTime } }, 'color courseName startTime', { sort: 'startTime' });
+    })
+    .then(function (signs) {      
+      var date;
+      var lastDate = moment(startTime);
+      var index = 0;
+      var calendar = [];
+      signs.forEach(function (sign) {
+        date = moment(sign.get('startTime'));
+        while (!date.isSame(lastDate, 'days')) {
+          index++;
+          lastDate.add(1, 'day');
+        }
+        if (!calendar[index]) {
+          calendar[index] = [];
+        }
+        calendar[index].push(sign.toObject());
+      });
+      calendar.length += moment(endTime).diff(lastDate, 'days') - 1;
+      sendInfo(errorCodes.Success, res, calendar);
+    })
+    .catch(function (err) {
+      if (err.code) {
+        sendInfo(err.code, res, []);
+      } else {
+        handleErrors(err, res, []);
+      }
+    });
 });
 
 module.exports = router;
