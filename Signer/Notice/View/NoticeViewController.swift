da73fa41a97c6050d8bbde3cca7da74c7b9904ba
VernonVan <1395872519@qq.com>
2017-03-10 11:46:35 +0800

    重构签到通知的代码

--- Signer/Notice/View/NoticeViewController.swift
+++ Signer/Notice/View/NoticeViewController.swift
@@ -46,65 +46,189 @@ class NoticeViewController: UIViewController
     var beforeNotices = [Notice]()      // 课前通知
     var totalBeforeNoticePage = 0
     var isLoadAllBeforeNotices = false  // 所有课前通知都已经加载完
-
+    
     var afterNotices = [Notice]()       // 课后通知
     var totalAfterNoticePage = 0
     var isLoadAllAfterNotices = false
-
-    lazy var refreshControl: UIRefreshControl = {
-        let refreshControl = UIRefreshControl()
-        refreshControl.tintColor = UIColor(netHex: 0x97cc00)
-        refreshControl.addTarget(self, action: #selector(refreshNoticesFromService), for: .valueChanged)
-        return refreshControl
-    }()
+    
+    
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        monitorToNetChange()
-
-        tableView.delegate = self
         tableView.dataSource = self
+        tableView.delegate = self
         tableView.emptyDataSetSource = self
         tableView.emptyDataSetDelegate = self
         tableView.addSubview(refreshControl)
-    
+        
         loadNoticesFor(type: .beforeClass, page: 0)
         loadNoticesFor(type: .afterClass, page: 0)
-        
-        // 下拉刷新
-//        tableView.addPullToRefresh { [unowned self] in
-//            self.refreshNoticesFromServiceFor(type: self.noticeType)
-//        }
-//        tableView.pullToRefreshView.setTitle("下拉刷新通知", forState: 0)
-//        tableView.pullToRefreshView.setTitle("释放更新通知", forState: 1)
-        
-        // 上拉加载
-        tableView.addInfiniteScrolling { [unowned self] in
-            if self.noticeType == .beforeClass {
-                if self.isLoadAllBeforeNotices {
-                    self.view.makeToast("无更多通知", duration: 0.3, position: .bottom)
-                    self.tableView.infiniteScrollingView.stopAnimating()
-                } else {
-                   self.loadNoticesFor(type: self.noticeType, page: self.totalBeforeNoticePage)
-                }
-            } else if self.noticeType == .afterClass {
-                if self.isLoadAllAfterNotices {
-                    self.view.makeToast("无更多通知", duration: 0.3, position: .bottom)
-                    self.tableView.infiniteScrollingView.stopAnimating()
-                } else {
-                    self.loadNoticesFor(type: self.noticeType, page: self.totalAfterNoticePage)
-                }
-            }
-        }
+
+        monitorToNetChange()
     }
     
     override func viewDidAppear(_ animated: Bool)
     {
         super.viewDidAppear(animated)
+        
+        self.navigationController?.tabBarItem.badgeValue = nil
+    }
 
-        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "ClearNoticeCountNotification"), object: nil)
+    // MARK: - 下拉刷新
+    
+    lazy var refreshControl: UIRefreshControl = {
+        let refreshControl = UIRefreshControl()
+        refreshControl.tintColor = ThemeGreenColor
+        refreshControl.addTarget(self, action: #selector(refreshNoticesFromService), for: .valueChanged)
+        return refreshControl
+    }()
+    
+    func refreshNoticesFromService()
+    {
+        self.navigationController?.tabBarItem.badgeValue = nil
+        
+        Alamofire
+            .request(StudentRouter.requestNotice(phone: Student().phone, type: noticeType.rawValue, page: 0))
+            .responseJSON { (response) in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    if json["code"] == "200" {
+                        DispatchQueue.global().async {
+                            self.refreshNoticesFor(type: self.noticeType, newNotices: self.convertJsonToNotices(json: json["data"]))
+                        }
+                    } else {
+                        fatalError("获取通知出错")
+                    }
+                case .failure(_):
+                    self.refreshControl.endRefreshing()
+                }
+        }
+    }
+    
+    func refreshNoticesFor(type: NoticeType, newNotices notices: [Notice])
+    {
+        if noticeType == .beforeClass {
+            beforeNotices.removeAll()
+            beforeNotices.append(contentsOf: notices)
+            totalBeforeNoticePage = 1
+            isLoadAllBeforeNotices = (notices.count < noticesNumberPerPage ? true : false)
+        } else if noticeType == .afterClass {
+            afterNotices.removeAll()
+            afterNotices.append(contentsOf: notices)
+            totalAfterNoticePage = 1
+            isLoadAllAfterNotices = (notices.count < noticesNumberPerPage ? true : false)
+        }
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+            self.tableView.reloadEmptyDataSet()
+            self.refreshControl.endRefreshing()
+            
+        }
+    }
+
+    // MARK: - 上拉加载更多通知
+    
+    func loadMoreNotice()
+    {
+        if noticeType == .beforeClass {
+            if isLoadAllBeforeNotices {
+                view.makeToast("无更多通知", duration: 0.3, position: .bottom)
+            } else {
+                loadNoticesFor(type: self.noticeType, page: self.totalBeforeNoticePage)
+            }
+        } else if noticeType == .afterClass {
+            if isLoadAllAfterNotices {
+                view.makeToast("无更多通知", duration: 0.3, position: .bottom)
+            } else {
+                loadNoticesFor(type: self.noticeType, page: self.totalAfterNoticePage)
+            }
+        }
+    }
+    
+    // 从服务器获取第page页的通知
+    func loadNoticesFor(type: NoticeType, page: Int)
+    {
+        Alamofire
+            .request(StudentRouter.requestNotice(phone: Student().phone, type: type.rawValue, page: page))
+            .responseJSON { (response) in
+                switch response.result {
+                case .success(let value):
+                    let json = JSON(value)
+                    if json["code"] == "200" {
+                        DispatchQueue.global().async {
+                            self.addNotices(newNotices: self.convertJsonToNotices(json: json["data"]), noticeType: type, page: page)
+                        }
+                    } else {
+                        fatalError("获取通知出错")
+                    }
+                case .failure(_):
+                    self.tableView.infiniteScrollingView.stopAnimating()
+                }
+        }
+    }
+    
+    lazy var dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        return dateFormatter
+    }()
+    
+    // 将服务器返回的JSON转化成notice数组
+    func convertJsonToNotices(json: JSON) -> [Notice]
+    {
+        var tempNotices = [Notice]()
+        
+        for (_, noticeJson) in json {
+            let courseName = noticeJson["courseName"].stringValue
+            let signNumber = noticeJson["signNumber"].intValue
+            let signState = SignState(num: noticeJson["signState"].intValue)!
+            let distance = noticeJson["signDistance"].intValue
+            let signDate = self.dateFormatter.date(from: noticeJson["signAt"].stringValue)!
+            let confirmDate = self.dateFormatter.date(from: noticeJson["confirmAt"].stringValue)!
+            
+            let notice = Notice(courseName: courseName, signNumber: signNumber, signState: signState, distance: distance, signDate: signDate, confirmDate: confirmDate)
+            tempNotices.append(notice)
+        }
+        
+        return tempNotices
+    }
+    
+    // 将网页中获取到的第page页的新通知加到数据源中，更新tableView
+    func addNotices(newNotices: [Notice], noticeType: NoticeType, page: Int)
+    {
+        var originalNoticeCount = 0
+        
+        if noticeType == .beforeClass {
+            originalNoticeCount = beforeNotices.count
+            beforeNotices.append(contentsOf: newNotices)
+            totalBeforeNoticePage = page + 1
+            isLoadAllBeforeNotices = (newNotices.count < noticesNumberPerPage ? true : false)
+        } else if noticeType == .afterClass {
+            originalNoticeCount = afterNotices.count
+            afterNotices.append(contentsOf: newNotices)
+            totalAfterNoticePage = page + 1
+            isLoadAllAfterNotices = (newNotices.count < noticesNumberPerPage ? true : false)
+        }
+        
+        if self.noticeType == noticeType {
+            var appendIndexPaths = [IndexPath]()
+            for i in 0 ..< newNotices.count {
+                appendIndexPaths.append(IndexPath(row: originalNoticeCount + i, section: 0))
+            }
+            
+            DispatchQueue.main.async {
+                self.tableView.insertRows(at: appendIndexPaths, with: UITableViewRowAnimation.fade)
+            }
+        }
+    }
+    
+    @IBAction func changeNoticeTypeAction(_ segmentedControl: UISegmentedControl)
+    {
+        noticeType = NoticeType(rawValue: segmentedControl.selectedSegmentIndex)!
     }
     
     // 监测网络状态变化
@@ -132,130 +256,6 @@ class NoticeViewController: UIViewController
         reach!.startNotifier()
     }
     
-    // MARK: - pull to refresh
-    
-    // 下拉刷新响应事件
-    func refreshNoticesFromService()
-    {
-        Alamofire
-            .request(StudentRouter.requestNotice(phone: Student().phone, type: noticeType.rawValue, page: 0))
-            .responseJSON { (response) in
-                switch response.result {
-                case .success(let value):
-                    let json = JSON(value)
-                    if json["code"] == "200" {
-                        DispatchQueue.global().async {
-                            self.refreshNoticesFor(type: self.noticeType, newNotices: self.convertJsonToNotices(json: json["data"]))
-                        }
-                    } else {
-                        fatalError("获取通知出错")
-                    }
-                case .failure(_):
-                    self.refreshControl.endRefreshing()
-                }
-            }
-    }
-    
-    func refreshNoticesFor(type: NoticeType, newNotices notices: [Notice])
-    {
-//        print("刷新\(notices.count)条通知")
-        
-        if noticeType == .beforeClass {
-            beforeNotices.removeAll()
-            beforeNotices.append(contentsOf: notices)
-            totalBeforeNoticePage = 1
-            isLoadAllBeforeNotices = (notices.count<noticesNumberPerPage ? true : false)
-        } else if noticeType == .afterClass {
-            afterNotices.removeAll()
-            afterNotices.append(contentsOf: notices)
-            totalAfterNoticePage = 1
-            isLoadAllAfterNotices = (notices.count<noticesNumberPerPage ? true : false)
-        }
-        
-        DispatchQueue.main.async {
-            self.tableView.reloadData()
-            self.tableView.reloadEmptyDataSet()
-            self.refreshControl.endRefreshing()
-        }
-    }
-    
-    // MARK: - pull up to load
-    
-    // 从服务器获取第page页的通知
-    func loadNoticesFor(type: NoticeType, page: Int)
-    {
-        Alamofire
-            .request(StudentRouter.requestNotice(phone: Student().phone, type: type.rawValue, page: page))
-            .responseJSON { (response) in
-                switch response.result {
-                case .success(let value):
-                    let json = JSON(value)
-//                    print("获取通知: \(json)")
-                    if json["code"] == "200" {
-                        DispatchQueue.global().async {
-                            self.addNotices(newNotices: self.convertJsonToNotices(json: json["data"]), noticeType: type, page: page)
-                        }
-                    } else {
-                        fatalError("获取通知出错")
-                    }
-                case .failure(_):
-                    self.tableView.infiniteScrollingView.stopAnimating()
-                }
-        }
-    }
-    
-    lazy var dateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        return dateFormatter
-    }()
-    
-    // 将服务器返回的JSON转化成notice数组
-    func convertJsonToNotices(json: JSON) -> [Notice]
-    {
-        var tempNotices = [Notice]()
-
-        for (_, noticeJson) in json {
-            let courseName = noticeJson["courseName"].stringValue
-            let signNumber = noticeJson["signNumber"].intValue
-            let signState = SignState(num: noticeJson["signState"].intValue)!
-            let distance = noticeJson["signDistance"].intValue
-            let signDate = self.dateFormatter.date(from: noticeJson["signAt"].stringValue)!
-            let confirmDate = self.dateFormatter.date(from: noticeJson["confirmAt"].stringValue)!
-            
-            let notice = Notice(courseName: courseName, signNumber: signNumber, signState: signState, distance: distance, signDate: signDate, confirmDate: confirmDate)
-            tempNotices.append(notice)
-        }
-
-        return tempNotices
-    }
-    
-    // 将网页中获取到的第page页的新通知加到数据源中，更新tableView
-    func addNotices(newNotices: [Notice], noticeType: NoticeType, page: Int)
-    {
-      //  print("新加载\(newNotices.count)条通知")
-
-        if noticeType == .beforeClass {
-            beforeNotices.append(contentsOf: newNotices)
-            totalBeforeNoticePage = page + 1
-            isLoadAllBeforeNotices = (newNotices.count<noticesNumberPerPage ? true : false)
-        } else if noticeType == .afterClass {
-            afterNotices.append(contentsOf: newNotices)
-            totalAfterNoticePage = page + 1
-            isLoadAllAfterNotices = (newNotices.count<noticesNumberPerPage ? true : false)
-        }
-        
-        DispatchQueue.main.async {
-            self.tableView.reloadData()
-            self.tableView.infiniteScrollingView.stopAnimating()
-        }
-    }
-
-    @IBAction func changeNoticeTypeAction(_ segmentedControl: UISegmentedControl)
-    {
-        noticeType = NoticeType(rawValue: segmentedControl.selectedSegmentIndex)!
-    }
-    
 }
 
 extension NoticeViewController: UITableViewDataSource, UITableViewDelegate
@@ -264,9 +264,10 @@ extension NoticeViewController: UITableViewDataSource, UITableViewDelegate
     {
         if noticeType == .beforeClass {
             return beforeNotices.count
-        } else {
+        } else if noticeType == .afterClass {
             return afterNotices.count
         }
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
@@ -286,9 +287,20 @@ extension NoticeViewController: UITableViewDataSource, UITableViewDelegate
     {
         return NoticeCell.cellHeight
     }
+    
+    // 加载更多
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath)
+    {
+        let noticeCount = noticeType == .beforeClass ? beforeNotices.count : afterNotices.count
+        if noticeCount - 1 == indexPath.row {
+            loadMoreNotice()
+        }
+    }
+    
+    
 }
 
-// MARK: - empty table
+// MARK: - Empty table
 
 extension NoticeViewController: DZNEmptyDataSetSource, DZNEmptyDataSetDelegate
 {
